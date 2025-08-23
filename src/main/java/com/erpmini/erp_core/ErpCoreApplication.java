06b7921da0da5def114de60f87b62a046b93e92d
Hammond <Hieutruonghoang01@gmail.com>
2025-08-23 10:09:57 +0700

    Initial project setup: base structure, configs, and core files

---
+++ src/main/java/com/erpmini/erp_core/ErpCoreApplication.java
@@ -0,0 +1,13 @@
+package com.erpmini.erp_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpCoreApplication.class, args);
+	}
+
+}
